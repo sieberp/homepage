4a18101a48c1a70c233a48d69360b4966f1e38ac
Paul Sieber <paul.sieber@gmx.at>
2021-02-28 21:31:47 +0100

    feat: email sending functionality set up

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,64 @@
 import React from 'react';
+import axios from 'axios';
 
 import SEO from '../components/seo';
 
-const Contact = () => (
-  <>
-    <SEO title="Contact" keywords={[`rowing`, `olympics`, `sieber`, `paul`]} />
-    <h1>CONTACT</h1>
-    <p>Here should be a contact form that sends emails to me</p>
-  </>
-);
+axios.defaults.baseURL = '/';
+
+const Contact = () => {
+  const [inputs, setInputs] = React.useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [buttonText, setButtonText] = React.useState('Send!');
+
+  const handleChange = event => {
+    setInputs({
+      ...inputs,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const sendMail = async event => {
+    event.preventDefault();
+    console.log(inputs);
+    setButtonText('Sending...');
+    console.log(axios.defaults);
+    const response = await axios.post(
+      '/.netlify/functions/send-contact-email',
+      {
+        contactName: inputs.name,
+        contactEmail: inputs.email,
+        message: inputs.message,
+      }
+    );
+    setInputs({
+      name: '',
+      email: '',
+      message: '',
+    });
+    setButtonText('Send!');
+    console.log(response);
+  };
+
+  return (
+    <>
+      <SEO
+        title="Contact"
+        keywords={[`rowing`, `olympics`, `sieber`, `paul`]}
+      />
+      <h1>CONTACT</h1>
+      <form action="">
+        <input type="email" id="email" name="email" onChange={handleChange} />
+        <input type="text" id="name" name="name" onChange={handleChange} />
+        <textarea id="message" name="message" onChange={handleChange} />
+        <button type="submit" onClick={sendMail}>
+          {buttonText}
+        </button>
+      </form>
+    </>
+  );
+};
 
 export default Contact;
