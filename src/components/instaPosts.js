98bdb77a0972583697402b3d653a9b65d8415c72
Paul Sieber <paul.sieber@gmx.at>
2021-03-02 12:39:51 +0100

    fix: instaPosts fixed

--- src/components/instaPosts.js
+++ src/components/instaPosts.js
@@ -23,43 +23,42 @@ const InstaWrapper = styled.div`
 
 function InstaPosts() {
   return (
-    // <StaticQuery
-    //   query={graphql`
-    //     query InstaQuery {
-    //       allInstaNode(
-    //         limit: 6
-    //         # sort: { order: DESC, fields: timestamp }
-    //         filter: { mediaType: { eq: "GraphImage" } }
-    //       ) {
-    //         edges {
-    //           node {
-    //             id
-    //             caption
-    //             thumbnails {
-    //               config_height
-    //               config_width
-    //               src
-    //             }
-    //           }
-    //         }
-    //       }
-    //     }
-    //   `}
-    //   render={data => {
-    //     return (
-    //       <InstaWrapper>
-    //         {data.allInstaNode.edges.map(edge => (
-    //           <InstaImage
-    //             src={edge.node.thumbnails[3].src}
-    //             key={edge.node.id}
-    //             width={edge.node.thumbnails[3].config_width}
-    //           />
-    //         ))}
-    //       </InstaWrapper>
-    //     );
-    //   }}
-    // />
-    <h1>Here should be the Insta Posts</h1>
+    <StaticQuery
+      query={graphql`
+        query InstaQuery {
+          allInstaNode(
+            limit: 6
+            # sort: { order: DESC, fields: timestamp }
+            filter: { mediaType: { eq: "GraphImage" } }
+          ) {
+            edges {
+              node {
+                id
+                caption
+                thumbnails {
+                  config_height
+                  config_width
+                  src
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        return (
+          <InstaWrapper>
+            {data.allInstaNode.edges.map(edge => (
+              <InstaImage
+                src={edge.node.thumbnails[3].src}
+                key={edge.node.id}
+                width={edge.node.thumbnails[3].config_width}
+              />
+            ))}
+          </InstaWrapper>
+        );
+      }}
+    />
   );
 }
 
